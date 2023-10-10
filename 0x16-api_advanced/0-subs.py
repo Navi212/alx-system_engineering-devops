253ada18f9a65cf76f13aa010ddd5444463811bd
Joseph Nweke <josephchristo615@gmail.com>
2023-10-10 23:15:45 +0100

    Adds updated copy task0 file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Queries the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    """Queries the Reddit API and returns the number of subscribers"""
+    headers = {"User-Agent": "Chrome/117.0.0.0"}
+    url = f"https://www.reddit.com/r/{subreddit}/.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+        return (0)
+    result = response.json().get("data")
+    sub = result.get("subscribers")
+    return (sub)
