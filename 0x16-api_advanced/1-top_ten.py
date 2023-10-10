be138300d1d255773989dd6a34a6ea9acef377a9
Joseph Nweke <josephchristo615@gmail.com>
2023-10-11 00:32:15 +0100

    Adds updated task1 file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""A 1-top_ten module"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """Queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Chrome 199929"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    result = response.json().get("data")
+    if (response.status_code == 404):
+        return (0)
+    li = []
+    for i in result.get("children"):
+        li.append(i.get("data").get("title"))
+    print(li)
