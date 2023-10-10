0ebdc152f2f9c7db7527b6923e2f723a2152928a
Joseph Nweke <josephchristo615@gmail.com>
2023-10-11 00:45:44 +0100

    Adds test file and task1 file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""A 1-top_ten module"""
+"""
+A module that supplies a function that queries Reddit API and prints
+first10 hot posts listed for a given subreddit.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot
     posts listed for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Chrome 199929"}
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
+    headers = {"User-Agent": "Chrome/117.0.0.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     result = response.json().get("data")
     if (response.status_code == 404):
         return (0)
-    li = []
     for i in result.get("children"):
-        li.append(i.get("data").get("title"))
-    print(li)
+        print(i.get("data").get("title"))
