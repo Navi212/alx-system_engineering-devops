47ea3826c9168592be8a9027cc7b59e5c6d5ba25
Joseph Nweke <josephchristo615@gmail.com>
2023-10-19 13:03:07 +0100

    Updates task0 file adding systemctl

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # A puppet manifest that Increases the amount of traffic an Nginx server can handle.
 
 exec {'update max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo systemctl reload nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
